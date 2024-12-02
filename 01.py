25b3be84ee3ab0fd5a4f39094b148ed6bcbb0346
burger20042004 <jalindranath2004.sanjivaniit@gmail.com>
2024-12-02 08:50:46 +0530

    Add files via upload

--- 01.py
+++ 01.py
@@ -1,9 +1,3 @@
-##Assignment No.01##
-#Name:Rahane Shital Anna
-##Roll No:48
-#Batch:B3
-#Title:"Text Pre-Processing using NLP operations:perform Tokenization
-# Stop word removal,Lemmatization ,Part-of-Speech Tagging use any sample text"
 
 #import libraries
 import spacy
@@ -46,68 +40,3 @@ TOKEN: {str(token)}
 TAG: {str(token.tag_):10} POS: {token.pos_}
 EXPLANATION: {spacy.explain(token.tag_)}"""
     )
-
-
-#----------output-------#
-"""India 0
-is 6
-my 9
-country 12
-. 18
-Maharashtra 20
-is 32
-my 35
-state 38
-. 43
-[India, country, ., Maharashtra, state, .]
-is : be
-is : be
-TOKEN: India
-=====
-TAG: NNP        POS: PROPN
-EXPLANATION: noun, proper singular
-
-TOKEN: is
-=====
-TAG: VBZ        POS: AUX
-EXPLANATION: verb, 3rd person singular present
-
-TOKEN: my
-=====
-TAG: PRP$       POS: DET
-EXPLANATION: pronoun, possessive
-
-TOKEN: country
-=====
-TAG: NN         POS: NOUN
-EXPLANATION: noun, singular or mass
-
-TOKEN: .
-=====
-TAG: .          POS: PUNCT
-EXPLANATION: punctuation mark, sentence closer
-
-TOKEN: Maharashtra
-=====
-TAG: NNP        POS: PROPN
-EXPLANATION: noun, proper singular
-
-TOKEN: is
-=====
-TAG: VBZ        POS: AUX
-EXPLANATION: verb, 3rd person singular present
-
-TOKEN: my
-=====
-TAG: PRP$       POS: DET
-EXPLANATION: pronoun, possessive
-
-TOKEN: state
-=====
-TAG: NN         POS: NOUN
-EXPLANATION: noun, singular or mass
-
-TOKEN: .
-=====
-TAG: .          POS: PUNCT
-EXPLANATION: punctuation mark, sentence closer"""
